e9cc129f9449c92393ef37b78a98cfee0177f55a
Arman Dezfuli <armandezfuli@proton.me>
2024-01-15 09:20:02 +0330

    change show com

--- src/scripts/Show.js
+++ src/scripts/Show.js
@@ -1,35 +1,31 @@
 import { Api, Element } from "./utility.js";
 
-const apiDetails = {
-  method: "GET",
-  url: "https://users-f76be-default-rtdb.firebaseio.com/users.json",
-};
-
 class Show {
-  static async getData(props) {
-    Show.data = Object.entries(await Api.get(props));
-    Show.render(Show.data);
+  constructor(props) {
+    this.apiDetails = props;
+    this.tbody = Element.get("tbody");
   }
 
-  static render(props) {
-    const tbody = Element.get("tbody");
-    tbody.innerHTML = "";
-
-    for (const item of props) {
-      const tr = `
+  async reander() {
+    this.tbody.innerHTML = "";
+    try {
+      const data = Object.entries(await Api.get(this.apiDetails));
+      for (const item of data) {
+        const tr = `
           <tr>
-          <td class="userid">${item[0]}</td>
-          <td>${item[1].firstName}</td>
-          <td>${item[1].lastName}</td>
-          <td>${item[1].username}</td>
-          <td class="edBtn">Edite</td>
-          <td class="delBtn">Delete</td>
+            <td class="userid">${item[0]}</td>
+            <td>${item[1].firstName}</td>
+            <td>${item[1].lastName}</td>
+            <td>${item[1].username}</td>
+            <td class="edBtn">Edit</td>
+            <td class="delBtn">Delete</td>
           </tr>`;
-      tbody.insertAdjacentHTML("beforeend", tr);
+        this.tbody.insertAdjacentHTML("beforeend", tr);
+      }
+    } catch (error) {
+      console.error(error);
     }
   }
 }
-
-Show.getData(apiDetails);
 
 export { Show };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,3 +2,12 @@ import { Api } from "./utility.js";
 import { Show } from "./Show.js";
 import { AddUser } from "./AddUser.js";
 
+const apiDetails = {
+  method: "GET",
+  url: "https://users-f76be-default-rtdb.firebaseio.com/users.json",
+  body: null
+};
+
+
+const showInstance = new Show(apiDetails);
+showInstance.reander();
